6ffdd15f698013cad733750e1208c0370bcb4606
trahern <trahern@83c7745e-aeb2-70ca-e83f-ce57b7db7bff>
2012-04-09 16:36:44 +0000

    look ma a change
    git-svn-id: https://github.com/mulkieran/repo/trunk@4 83c7745e-aeb2-70ca-e83f-ce57b7db7bff

--- interp.rkt
+++ interp.rkt
@@ -15,8 +15,24 @@
   (num 0))
 
 ;; unparse: WAE -> s-exp
-(define (unparse a-wae)
-  '0)
+(define (unparse an-wae)
+  (type-case WAE an-wae
+    [num (n) n]
+    [id (n) n]
+    [with (n e b)
+          (list 'with
+                (list n (unparse e))
+                (unparse b))]
+    [add (l r)
+         (list '+ (unparse l) (unparse r))]
+    [sub (l r)
+         (list '- (unparse l) (unparse r))]))  
+(test (unparse (num 3)) '3)
+(test (unparse (add (num 3) (num 7)))
+      (list '+ 3 7))
+(test (unparse (with 'x (num 1) (add (id 'x) (num 7))))
+      (list 'with (list 'x 1) (list '+ 'x 7)))
+(test (unpar
 
 ;; random-wae : number -> WAE
 (define (random-wae depth)
